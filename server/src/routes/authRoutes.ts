2dcdb50a414735540c697e8c9108f80b4152d5b6
Daksh Purohit <dakshgrows@gmail.com>
2025-06-24 00:17:03 +0530

    BE

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -13,6 +13,7 @@ router.post("/signup", async (req: Request, res: Response): Promise<void> => {
         const existingUser = await UserModel.findOne({ email });
         if (existingUser) {
             res.status(400).json({ message: "User already exists" });
+            return;
         }
     
         const hashedPassword = await bcrypt.hash(password, 10)
